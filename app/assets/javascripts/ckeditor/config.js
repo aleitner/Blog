7f3b7cb214a5fd40f311a3b1156a82ae26115b24
# <#>
2014-08-22 00:07:18 -0400

    custom wysiwig

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,6 @@
+CKEDITOR.editorConfig = function(config) {
+    config.toolbar = [
+        {name: "basicstyles", items: ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript", "-", "RemoveFormat"]},
+        {name: "paragraph", items: ["NumberedList", "BulletedList"]}
+    ];
+}
