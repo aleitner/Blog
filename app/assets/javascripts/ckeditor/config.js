c33850ac344774510a5325bc312feac929814148
# <#>
2014-09-01 12:04:43 -0400

    editted ckeditor layout

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,13 @@
 CKEDITOR.editorConfig = function(config) {
     config.toolbar = [
-        {name: "basicstyles", items: ["Bold", "Italic", "Underline", "Strike", "Subscript", "Superscript", "-", "RemoveFormat"]},
-        {name: "paragraph", items: ["NumberedList", "BulletedList"]}
+    { name: 'document',    items : [ 'Source' ] },
+    { name: 'insert',      items : [ 'Image','Table','HorizontalRule','SpecialChar' ] },
+    { name: 'links',       items : [ 'Link','Unlink' ] },
+    '/',
+    { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+    { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+    '/',
+    { name: 'styles',      items : [ 'Styles','Format','Font','FontSize' ] },
+    { name: 'colors',      items : [ 'TextColor','BGColor' ] }
     ];
 }
